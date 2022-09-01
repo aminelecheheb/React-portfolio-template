de59afb84a2507c1ef1c3f702018d988146d978c
Amine Lecheheb <aminelch01@gmail.com>
2022-09-01 21:01:34 +0100

    initial hero section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "./header.css";
-import CV from "../../assets/cv.pdf";
-import ME from "../../assets/me.png";
 import HeaderSocials from "./HeaderSocials";
+import STAR from "../../assets/star.png";
+import LAPTOP from "../../assets/laptop.png";
 
 const Header = () => {
   return (
     <header>
       <div className="container header__container">
+        <img className="star" src={STAR} alt="star" />
+
+        <div className="string"></div>
+
+        <h1 className="hero-title">
+          Create beautiful, unique, <br /> responsive and fast websites <br />{" "}
+          with <span>CODAMINE</span>
+        </h1>
         <HeaderSocials />
         <a href="#contact" className="scroll__down">
           Scroll Down
